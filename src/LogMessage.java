b77bcb2898b4c00386ef788bdd10a8a5f6fae248
pleiotropy <angela.m.joseph+github@gmail.com>
2023-05-07 13:51:43 -0400

    U5L9 Scope and Access - LogMessage works now

--- src/LogMessage.java
+++ src/LogMessage.java
@@ -10,24 +10,33 @@ public class LogMessage {
     }
     public boolean containsWord(String keyword) {
         // description is exactly the keyword
-        if (description.equals(keyword)) {
-            return true;
+        if (description.indexOf(keyword) != -1) {
+            if (description.equals(keyword)) {
+                System.out.print("exactly the keyword: ");
+                return true;
+            }
+            // at beginning and followed by space
+            else if (description.indexOf(keyword + " ") == 0) {
+                System.out.print("at beginning and followed by a space: ");
+                return true;
+            }
+            // at end and preceded by a space
+            else if (description.indexOf(" " + keyword) == description.length() - (keyword.length() + 1)) {
+                System.out.print("at end and preceded by a space: ");
+                return true;
+            }
+            // in the middle and surrounded by spaces
+            else if (description.indexOf(" " + keyword + " ") != -1) {
+                System.out.print("in the middle surrounded by spaces: ");
+                return true;
+            }
+            else {
+                System.out.print("keyword not found: ");
+                return false;
+            }
         }
-        // at beginning and followed by space
-        else if (description.indexOf(keyword + " ") == 0) {
-            return true;
-        }
-        // at end and preceded by a space
-        else if (description.indexOf(" " + keyword) == description.length() - (keyword.length() + 1)) {
-            return true;
-        }
-        // in the middle and surrounded by spaces
-        else if (description.indexOf(" " + keyword + " ") != -1) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        System.out.print("keyword not found. didnt even check: ");
+        return false;
     }
 
     public String getMachineId() {
