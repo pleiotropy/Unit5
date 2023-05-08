cfd923c32bafaff1b6898cd8ae72a84972f0c6ad
pleiotropy <angela.m.joseph+github@gmail.com>
2023-05-07 22:40:51 -0400

    U5L10 this Keyword

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,56 @@ public class Main
 {
     public static void main(String[] args)
     {
-        LogMessage test1 = new LogMessage("CLIENT3:security alert - repeated login failures");
-        LogMessage test2 = new LogMessage("Webserver:disk offline");
-        LogMessage test3 = new LogMessage("SERVER1:file not found");
-        LogMessage test4 = new LogMessage("SERVER2:read error on disk DSK1");
-        LogMessage test5 = new LogMessage("SERVER1:write error on disk DSK2");
-        LogMessage test6 = new LogMessage("Webserver:error on /dev/disk");
-        LogMessage test7 = new LogMessage("7:disk");
-        LogMessage test8 = new LogMessage("8:error on disk");
-        LogMessage test9 = new LogMessage("9:error on /dev/disk disk");
-        LogMessage test10 = new LogMessage("10:error on disk DSK1");
-        LogMessage test11 = new LogMessage("11:DISK");
-        LogMessage test12 = new LogMessage("12:error on disk3");
-        LogMessage test13 = new LogMessage("13:error on /dev/disk");
-        LogMessage test14 = new LogMessage("14:diskette");
+        Fraction frac1 = new Fraction(7, 8);
+        System.out.println(frac1);
 
-        System.out.println("1: " + test1.containsWord("disk"));
-        System.out.println("2: " + test2.containsWord("disk"));
-        System.out.println("3: " + test3.containsWord("disk"));
-        System.out.println("4: " + test4.containsWord("disk"));
-        System.out.println("5: " + test5.containsWord("disk"));
-        System.out.println("6: " + test6.containsWord("disk"));
-        System.out.println("7: " + test7.containsWord("disk"));
-        System.out.println("8: " + test8.containsWord("disk"));
-        System.out.println("9: " + test9.containsWord("disk"));
-        System.out.println("10: " + test10.containsWord("disk"));
-        System.out.println("11: " + test11.containsWord("disk"));
-        System.out.println("12: " + test12.containsWord("disk"));
-        System.out.println("13: " + test13.containsWord("disk"));
-        System.out.println("14: " + test14.containsWord("disk"));
+        Fraction frac2 = new Fraction(6);
+        System.out.println(frac2);
+
+        Fraction frac3 = new Fraction();
+        System.out.println(frac3);
+
+        Fraction frac4 = new Fraction(2, 4);
+        Fraction frac5 = frac4.multiplyWithFraction(frac1);
+        System.out.println(frac5);
+        System.out.println(frac1);
+
+        Fraction frac6 = new Fraction(9, 14);
+        frac6.flip();
+        System.out.println(frac6);
+
+        System.out.println("----");
+
+        // watch the references!
+        Fraction frac7 = new Fraction(3, 4);
+        Fraction bigger = frac7.largerFraction(frac4);
+        System.out.println(bigger);
+        bigger.flip();
+        System.out.println(frac7);
+
+        Fraction frac8 = new Fraction(2);
+        bigger = frac7.largerFraction(frac8);
+        System.out.println(bigger);
+        bigger.flip();
+        System.out.println(frac8);
+
+        System.out.println("----");
+
+        Fraction frac9 = new Fraction(1, 2);
+        Fraction frac10 = new Fraction(2, 3);
+
+        double productAsDouble = Fraction.multiplyFractions(frac9, frac10).asDecimal();
+        System.out.println(productAsDouble);
+
+        System.out.println("----");
+        Fraction largest = frac1.largerFraction(frac2).largerFraction(frac3).largerFraction(frac4);
+        System.out.println(largest);
+
+        System.out.println("----");
+        double srslyWTFdoesThisDo =
+                Fraction.multiplyFractions(frac9, (frac9.largerFraction(frac10).multiplyWithFraction(new Fraction(6, 4)).largerFraction(frac9))).asDecimal();
+
+        System.out.println(srslyWTFdoesThisDo);
+
     }
 }
